e7bc42206d7fae082e11cdaa4b3b193a3053de1c
Fabio Aringoli <aringolifabio@gmail.com>
2023-02-17 15:28:19 -0300

    ?

--- JS/Script.js
+++ JS/Script.js
@@ -1,15 +1,3 @@
-//function EnviarMail() {
-    //var params = {
-       // from_name : document.getElementById("from_name").value,
-        //email_id : document.getElementById("email_id").value,
-        //message : document.getElementById("message").value
-    //}
-    //emailjs.send("service_zx759un", "template_d4kpijp", params).then(function (res) {
-       // alert("Mensaje enviado " + res.status);
-    //})
-//}
-
-
 const btn = document.getElementById('button');
 
 document.getElementById('form')
